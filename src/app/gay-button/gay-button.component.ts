65eb7bb97758e2877c762ae572a148847113ae73
Joao Dias <joao.dias@cherrygroup.com>
2017-11-16 15:59:53 +0100

    add a red button component

---
+++ src/app/gay-button/gay-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button.component';
+
+@Component({
+  selector: 'red-gay-button',
+  template: '<button>Red Gay</button>',
+})
+export class GayButtonComponent extends ButtonComponent {
+}
